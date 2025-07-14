7e65a64e3dbb7841e627d7e1f894f248ca60f37a
Yoga Sulistiyo Widodo <chaintroopers88@gmail.com>
2025-07-14 22:46:31 +0700

    feat: create interface and implementation for user service and also manualy added Bcrypt for hashing

--- src/main/java/programmer_zaman_now/belajar_spring_restful_api/service/UserService.java
+++ src/main/java/programmer_zaman_now/belajar_spring_restful_api/service/UserService.java
@@ -1,4 +1,13 @@
 package programmer_zaman_now.belajar_spring_restful_api.service;
 
-public class UserService {
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import programmer_zaman_now.belajar_spring_restful_api.model.request.RegisterUserRequest;
+import programmer_zaman_now.belajar_spring_restful_api.repository.UserRepository;
+
+
+public interface UserService {
+
+    @Transactional
+    void register(RegisterUserRequest request);
 }
--- src/main/java/programmer_zaman_now/belajar_spring_restful_api/service/impl/UserImpl.java
+++ src/main/java/programmer_zaman_now/belajar_spring_restful_api/service/impl/UserImpl.java
@@ -1,4 +1,50 @@
 package programmer_zaman_now.belajar_spring_restful_api.service.impl;
 
-public class UserImpl {
+import jakarta.transaction.Transactional;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Validator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import programmer_zaman_now.belajar_spring_restful_api.entity.User;
+import programmer_zaman_now.belajar_spring_restful_api.model.request.RegisterUserRequest;
+import programmer_zaman_now.belajar_spring_restful_api.repository.UserRepository;
+import programmer_zaman_now.belajar_spring_restful_api.security.BCrypt;
+import programmer_zaman_now.belajar_spring_restful_api.service.UserService;
+
+import java.util.Set;
+
+@Service
+public class UserImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @Autowired
+    private Validator validator;
+
+    @Override
+    public void register(RegisterUserRequest request) {
+        Set<ConstraintViolation<RegisterUserRequest>> constraintViolations = validator.validate(request);
+
+        if (!constraintViolations.isEmpty()){
+            throw new ConstraintViolationException(constraintViolations);
+        }
+
+        if (userRepository.existsById(request.getUsername())){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Username already exists");
+        }
+
+        User user = new User();
+        user.setUsername(request.getUsername());
+        user.setPassword(BCrypt.hashpw(request.getPassword(), BCrypt.gensalt()));
+        user.setName(request.getName());
+
+        userRepository.save(user);
+
+
+    }
 }
