0191b9732b4b512cd7f4fb8f00937b8552ff5f30
Yoga Sulistiyo Widodo <chaintroopers88@gmail.com>
2025-07-15 21:59:23 +0700

    feat: create ValidationService.java and ValidationImpl.java for validate request

--- src/main/java/programmer_zaman_now/belajar_spring_restful_api/service/ValidationService.java
+++ src/main/java/programmer_zaman_now/belajar_spring_restful_api/service/ValidationService.java
@@ -1,4 +1,8 @@
 package programmer_zaman_now.belajar_spring_restful_api.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public interface ValidationService {
+    void validate (Object request);
 }
--- src/main/java/programmer_zaman_now/belajar_spring_restful_api/service/impl/ValidationImpl.java
+++ src/main/java/programmer_zaman_now/belajar_spring_restful_api/service/impl/ValidationImpl.java
@@ -1,4 +1,26 @@
 package programmer_zaman_now.belajar_spring_restful_api.service.impl;
 
-public class ValidationImpl {
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Validator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import programmer_zaman_now.belajar_spring_restful_api.model.request.RegisterUserRequest;
+import programmer_zaman_now.belajar_spring_restful_api.service.ValidationService;
+
+import java.util.Set;
+
+@Service
+public class ValidationImpl implements ValidationService {
+
+    @Autowired
+    private Validator validator;
+
+    @Override
+    public void validate(Object request) {
+        Set<ConstraintViolation<Object>> constraintViolations = validator.validate(request);
+        if (!constraintViolations.isEmpty()){
+            throw new ConstraintViolationException(constraintViolations);
+        }
+    }
 }
