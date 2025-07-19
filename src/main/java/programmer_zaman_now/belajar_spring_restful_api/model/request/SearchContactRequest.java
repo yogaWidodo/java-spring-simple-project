63219236a89bf762cc9dc0799b482dbfe3ba7eec
Yoga Sulistiyo Widodo <chaintroopers88@gmail.com>
2025-07-19 17:30:53 +0700

    feat: add SearchContactRequest model for paginated and filtered contact search
    - Supports filtering by name, email, and phone
    - Includes pagination parameters: page and size (required)
    - Used for building search specifications in ContactService

---
+++ src/main/java/programmer_zaman_now/belajar_spring_restful_api/model/request/SearchContactRequest.java
@@ -0,0 +1,29 @@
+package programmer_zaman_now.belajar_spring_restful_api.model.request;
+
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class SearchContactRequest{
+
+    private String name;
+
+    private String email;
+
+    private String phone;
+
+    @NotNull
+    private Integer page;
+
+    @NotNull
+    private Integer size;
+
+}
