4a0e855b5c89462000fd62794afb68752a6f64bf
Yoga Sulistiyo Widodo <chaintroopers88@gmail.com>
2025-07-19 17:34:20 +0700

    feat: add generic WebResponse wrapper for standard API responses
    - Adds paging metadata through PagingResponse

--- src/main/java/programmer_zaman_now/belajar_spring_restful_api/model/WebResponse.java
+++ src/main/java/programmer_zaman_now/belajar_spring_restful_api/model/WebResponse.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Page;
+import programmer_zaman_now.belajar_spring_restful_api.model.response.PagingResponse;
 
 @Data
 @AllArgsConstructor
@@ -13,4 +15,5 @@ import lombok.NoArgsConstructor;
 public class WebResponse <T>{
     private T Data;
     private String errors;
+    private PagingResponse pagingResponse;
 }
