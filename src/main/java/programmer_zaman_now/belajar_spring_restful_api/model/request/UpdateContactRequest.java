d217d031a3e3924061e7c1b6a8f4fced54d7c3c4
Yoga Sulistiyo Widodo <chaintroopers88@gmail.com>
2025-07-19 17:31:18 +0700

    feat: add UpdateContactRequest model to support partial contact updates
    - Includes firstName (required), lastName, email, and phone (optional)
    - Uses @JsonIgnore on ID to prevent client from changing it via request body
    - ID i

---
+++ src/main/java/programmer_zaman_now/belajar_spring_restful_api/model/request/UpdateContactRequest.java
@@ -0,0 +1,34 @@
+package programmer_zaman_now.belajar_spring_restful_api.model.request;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UpdateContactRequest {
+    @JsonIgnore
+    @NotBlank
+    private String id;
+
+    @NotBlank
+    @Size(max = 100)
+    private String firstName;
+
+    @Size(max = 100)
+    private String lastName;
+
+    @Size(max = 100)
+    @Email
+    private String email;
+
+    @Size(max = 100)
+    private String phone;
+}
